1f2fef54c14b4af73beca1e365d7ffd5679500f9
wjchae <wjchae@softhills.net>
2024-01-12 12:37:58 +0900

    VIZWide3D

--- SHDC_Products/VIZWide3D/Writerside/cfg/gtag.js
+++ SHDC_Products/VIZWide3D/Writerside/cfg/gtag.js
@@ -5,7 +5,6 @@
 <script>
     window.addEventListener('load', () => {
         const test = document.querySelector('.footer__powered')
-        console.log(test.innerText);
         test.innerText = ''
     })
 </script>
